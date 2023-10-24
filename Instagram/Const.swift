20058a524754a2de2ca35dcb2fa20c726854bcd2
a.fujiki <a.fujiki@alphapolis.co.jp>
2023-10-24 17:59:54 +0900

    いいねぼたん、投稿の表示

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by WEBSYSTEM-MAC8 on 2023/10/24.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
